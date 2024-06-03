64a3a6acf85d3b97749743caa5fe698a282fc89b
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-02 21:35:42 -0300

    Mensagem de erro caso o email e ou a senha estejam errados

--- routes/entering.py
+++ routes/entering.py
@@ -1,15 +1,43 @@
 from flask import Blueprint, redirect, request, render_template, url_for
+from database.user import USERS
 
 
 entering = Blueprint("entering", __name__)
 
 
-@entering.route("/signin")
+@entering.route("/signin", methods=["GET", "POST"])
 def signin():
-    return "Sign In"
+    if request.method == "GET":
+        try:
+            error = request.args.get("error")
+            if error:
+               return render_template("signin.html", error=error) 
+        except:
+            ...
+        
+        return render_template("signin.html")
+
+    user = request.json
+
+    for client in USERS:
+        if user["email"] == client["email"] and \
+           user["password"] == client["password"]:
+            return redirect(url_for("entering.signup"))     
+            
+    return redirect(url_for("entering.signin", error=True))
 
 
-@entering.route("/signup")
+@entering.route("/certo")
+def certo(): 
+    return "<p>Tudo certo</p>"
+
+
+@entering.route("/errado")
+def errado():
+    return "<p>Senha e/ou email errado</p>"
+
+
+@entering.route("/signup", methods=["GET", "POST"])
 def signup():
-    return "Sign Up"
+    return render_template("signup.html")
 
